d154fe9360df4a4867982899b3ec3bfe4cdb6860
Ariel Piñeiro <ariel.pieiro@iskaypet.com>
2025-12-26 14:37:07 -0300

    fixes

--- src/main/java/com/iskaypet/core/Config.java
+++ src/main/java/com/iskaypet/core/Config.java
@@ -11,26 +11,21 @@ public final class Config {
 		// Utility class
 	}
 
-	private static Properties getProperties() {
-		return properties;
-	}
-
 	public static String getStringValue(String key) {
-		return getProperties().getProperty(key);
+		return properties.getProperty(key);
 	}
 
 	public static Long getLongValue(String key) {
-		return Long.valueOf(getProperties().getProperty(key));
+		return Long.valueOf(properties.getProperty(key));
 	}
 
 	public static Integer getIntValue(String key) {
-		return Integer.valueOf(getProperties().getProperty(key));
+		return Integer.valueOf(properties.getProperty(key));
 	}
 
 	public static java.util.Set<String> getRestClientNames() {
-		Properties props = getProperties();
 		Set<String> names = new java.util.HashSet<>();
-		for (String key : props.stringPropertyNames()) {
+		for (String key : properties.stringPropertyNames()) {
 			if (key.startsWith("rest.client.") && key.endsWith(".base.url")) {
 				String name = key.substring("rest.client.".length(),
 					key.length() - ".base.url".length());
